6c93b153e2a81efe493549c405fe0469b7f6f00f
Mark Beale <bealebeaverst@gmail.com>
2024-07-16 09:12:58 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//function upperCaseCustomerName(){
+//    System.out.println(customerName.toUpperCase());
+//    return customerName;
+//}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'karen';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
 // Write your solution in this file!
